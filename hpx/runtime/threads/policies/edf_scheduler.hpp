f3dcfe32ef223fb0de9ed4b953fecdb46c8a617d
Adrian Utrilla <adrianutrilla@gmail.com>
2018-01-21 20:27:24 +0100

    Remove unnecessary locks from scheduler

--- hpx/runtime/threads/policies/edf_scheduler.hpp
+++ hpx/runtime/threads/policies/edf_scheduler.hpp
@@ -64,7 +64,6 @@ namespace threads {
 
             edf_scheduler(std::size_t num_thread)
               : scheduler_base(num_thread, "edf_scheduler")
-              , queue_lock_()
               , memory_pool_(64)
             {
                 queue_ = std::make_unique<thread_queue_type>(1000);
@@ -73,7 +72,6 @@ namespace threads {
             edf_scheduler(init_parameter_type const& init,
                 bool deferred_initialization = true)
               : scheduler_base(init.num_queues_, "edf_scheduler")
-              , queue_lock_()
               , memory_pool_(64)
             {
                 queue_ = std::make_unique<thread_queue_type>(1000);
@@ -93,13 +91,11 @@ namespace threads {
 
             void abort_all_suspended_threads()
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 queue_->abort_all_suspended_threads();
             }
 
             bool cleanup_terminated(bool delete_all = false)
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 return queue_->cleanup_terminated(delete_all);
             }
 
@@ -107,7 +103,6 @@ namespace threads {
                 thread_state_enum initial_state, bool run_now, error_code& ec,
                 std::size_t num_thread)
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 queue_->create_thread(data, id, initial_state, run_now, ec);
             }
 
@@ -118,14 +113,12 @@ namespace threads {
 
             void peek_next_thread(threads::thread_data*& thrd)
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 return queue_->peek_next_thread(thrd);
             }
 
             virtual bool get_next_thread(std::size_t num_thread, bool running,
                 std::int64_t& idle_loop_count, threads::thread_data*& thrd)
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 return queue_->get_next_thread(thrd);
             }
 
@@ -133,7 +126,6 @@ namespace threads {
                 std::size_t num_thread,
                 thread_priority priority = thread_priority_normal)
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 queue_->schedule_thread(thrd);
             }
 
@@ -141,21 +133,18 @@ namespace threads {
                 std::size_t num_thread,
                 thread_priority priority = thread_priority_normal)
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 queue_->schedule_thread(thrd, true);
             }
 
             bool destroy_thread(
                 threads::thread_data* thrd, std::int64_t& busy_count)
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 return queue_->destroy_thread(thrd, busy_count);
             }
 
             std::int64_t get_queue_length(
                 std::size_t num_thread = std::size_t(-1)) const
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 return queue_->get_queue_length();
             }
 
@@ -164,7 +153,6 @@ namespace threads {
                 std::size_t num_thread = std::size_t(-1),
                 bool reset = false) const
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 // Return thread count of one specific queue.
                 std::int64_t count = 0;
                 if (std::size_t(-1) != num_thread)
@@ -223,14 +211,12 @@ namespace threads {
                 util::function_nonser<bool(thread_id_type)> const& f,
                 thread_state_enum state = unknown) const
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 return queue_->enumerate_threads(f, state);
             }
 
             virtual bool wait_or_add_new(std::size_t num_thread, bool running,
                 std::int64_t& idle_loop_count)
             {
-                std::lock_guard<std::mutex> lock{queue_lock_};
                 std::size_t added = 0;
                 return queue_->wait_or_add_new(running, idle_loop_count, added);
             }
@@ -246,7 +232,6 @@ namespace threads {
             }
 
         private:
-            mutable std::mutex queue_lock_;
             threads::thread_pool memory_pool_;
 
         protected:
